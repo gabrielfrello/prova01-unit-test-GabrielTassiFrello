04fbf4c47687519f62403f60c3583e4ffde4d1a6
gabrielfrello <gabrieltassifrello@hotmail.com>
2025-09-01 20:00:41 -0300

    commit

--- test/utilitarios.test.js
+++ test/utilitarios.test.js
@@ -1,105 +1,114 @@
-const Utilitarios = require('../src/utilitarios');
-const util = new Utilitarios();
-
-describe('Utilitarios', () => {
-  test('inverterString', () => {
-    expect(util.inverterString('abc')).toBe('cba');
-  });
-
-  test('contarCaracteres', () => {
-    expect(util.contarCaracteres('hello')).toBe(5);
-  });
-
-  test('paraMaiusculas', () => {
-    expect(util.paraMaiusculas('abc')).toBe('ABC');
-  });
-
-  test('paraMinusculas', () => {
-    expect(util.paraMinusculas('ABC')).toBe('abc');
-  });
-
-  test('primeiraLetraMaiuscula', () => {
-    expect(util.primeiraLetraMaiuscula('copilot')).toBe('Copilot');
-  });
-
-  test('somar', () => {
-    expect(util.somar(2, 3)).toBe(5);
-  });
-
-  test('subtrair', () => {
-    expect(util.subtrair(5, 2)).toBe(3);
-  });
-
-  test('multiplicar', () => {
-    expect(util.multiplicar(4, 3)).toBe(12);
-  });
-
-  test('dividir', () => {
-    expect(util.dividir(10, 2)).toBe(5);
-    expect(() => util.dividir(5, 0)).toThrow('Divisão por zero');
-  });
-
-  test('ehPar', () => {
-    expect(util.ehPar(4)).toBe(true);
-    expect(util.ehPar(5)).toBe(false);
-  });
-
-  test('primeiroElemento', () => {
-    expect(util.primeiroElemento([1, 2, 3])).toBe(1);
-  });
-
-  test('ultimoElemento', () => {
-    expect(util.ultimoElemento([1, 2, 3])).toBe(3);
-  });
-
-  test('tamanhoArray', () => {
-    expect(util.tamanhoArray([1, 2, 3])).toBe(3);
-  });
-
-  test('ordenarArray', () => {
-    expect(util.ordenarArray(['c', 'a', 'b'])).toEqual(['a', 'b', 'c']);
-  });
-
-  test('inverterArray', () => {
-    expect(util.inverterArray([1, 2, 3])).toEqual([3, 2, 1]);
-  });
-
-  test('ehNumero', () => {
-    expect(util.ehNumero(42)).toBe(true);
-    expect(util.ehNumero('42')).toBe(false);
-  });
-
-  test('removerEspacos', () => {
-    expect(util.removerEspacos('  texto  ')).toBe('texto');
-  });
-
-  test('repetirTexto', () => {
-    expect(util.repetirTexto('hi', 3)).toBe('hihihi');
-  });
-
-  test('juntarArray', () => {
-    expect(util.juntarArray(['a', 'b', 'c'], '-')).toBe('a-b-c');
-  });
-
-  test('contarPalavras', () => {
-    expect(util.contarPalavras('Olá mundo bonito')).toBe(3);
-  });
-
-  test('mediaArray', () => {
-    expect(util.mediaArray([2, 4, 6])).toBe(4);
-    expect(util.mediaArray([])).toBe(0);
-  });
-
-  test('removerDuplicados', () => {
-    expect(util.removerDuplicados([1, 2, 2, 3])).toEqual([1, 2, 3]);
-  });
-
-  test('ehPalindromo', () => {
-    expect(util.ehPalindromo('arara')).toBe(true);
-    expect(util.ehPalindromo('Copilot')).toBe(false);
-  });
-
-  test('mesclarObjetos', () => {
-    expect(util.mesclarObjetos({ a: 1 }, { b: 2 })).toEqual({ a: 1, b: 2 });
-  });
-});
+class Utilitarios {
+    inverterString(str) {
+      return str.split('').reverse().join('');
+    }
+  
+    contarCaracteres(str) {
+      return str.length;
+    }
+  
+    paraMaiusculas(str) {
+      return str.toUpperCase();
+    }
+  
+    paraMinusculas(str) {
+      return str.toLowerCase();
+    }
+  
+    primeiraLetraMaiuscula(str) {
+      if (!str) return '';
+      return str[0].toUpperCase() + str.slice(1);
+    }
+  
+    somar(a, b) {
+      return a + b;
+    }
+  
+    subtrair(a, b) {
+      return a - b;
+    }
+  
+    multiplicar(a, b) {
+      return a * b;
+    }
+  
+    dividir(a, b) {
+      if (b === 0) throw new Error('Divisão por zero');
+      return a / b;
+    }
+  
+    ehPar(num) {
+      return num % 2 === 0;
+    }
+  
+    primeiroElemento(arr) {
+      return arr[0];
+    }
+  
+    ultimoElemento(arr) {
+      return arr[arr.length - 1];
+    }
+  
+    tamanhoArray(arr) {
+      return arr.length;
+    }
+  
+    ordenarArray(arr) {
+      return [...arr].sort((a, b) => {
+        if (typeof a === 'number' && typeof b === 'number') {
+          return a - b;
+        }
+        return String(a).localeCompare(String(b));
+      });
+    }
+  
+    inverterArray(arr) {
+      return [...arr].reverse();
+    }
+  
+    gerarNumeroAleatorio(max = 100) {
+      return Math.floor(Math.random() * max);
+    }
+  
+    ehNumero(valor) {
+      return typeof valor === 'number';
+    }
+  
+    removerEspacos(str) {
+      return str.trim();
+    }
+  
+    repetirTexto(texto, vezes) {
+      return texto.repeat(vezes);
+    }
+  
+    juntarArray(arr, separador = ',') {
+      return arr.join(separador);
+    }
+  
+    contarPalavras(str) {
+      return str.trim().split(/\s+/).length;
+    }
+  
+    mediaArray(arr) {
+      if (arr.length === 0) return 0;
+      const soma = arr.reduce((acc, val) => acc + val, 0);
+      return soma / arr.length;
+    }
+  
+    removerDuplicados(arr) {
+      return [...new Set(arr)];
+    }
+  
+    ehPalindromo(str) {
+      const normalizado = str.toLowerCase();
+      return normalizado === normalizado.split('').reverse().join('');
+    }
+  
+    mesclarObjetos(obj1, obj2) {
+      return { ...obj1, ...obj2 };
+    }
+  }
+  
+  module.exports = Utilitarios;
+  
